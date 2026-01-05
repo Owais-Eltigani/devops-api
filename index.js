34d13f1fae54803a3ffbb5155e004a62c5f9651d
Dexter <ambidextrous.in@gmail.com>
2026-01-05 06:50:22 +0530

    project structure (#3)

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
-console.log("starting app...");
+import express from 'express';
+import dotenv from 'dotenv';
+const app = express();
+// Middleware to parse JSON requests
+app.use(express.json());
 
-setTimeout(() => {
-    console.log("2 seconds later...");
-}, 2000);
+// Load environment variables from .env file
+dotenv.config();
 
-console.log("app closed.");
+const PORT = process.env.PORT || 3000;
+
+
+
+// Sample route
+app.get('/', (req, res) => {
+    res.send('Hello, DevOps API!');
+});
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+    });
